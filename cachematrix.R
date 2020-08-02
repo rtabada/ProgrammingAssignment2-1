5bf5d0c85e385d099f00436af93fd28c6c254774
rtabada <67858915+rtabada@users.noreply.github.com>
2020-08-02 23:59:36 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,74 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
-## Write a short comment describing this function
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+
+## A function that creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(x = matrix()) {
+  invMat  <- NULL
+  set <- function(y){
+    x <<- y
+    invMat <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) invMat <<- inverse
+  getInverse <- function() invMat   
+  list(set = set, 
+       get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+}
+
+## A function that computes the inverse of the special "matrix" created by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  invMat <- x$getInverse()
+  if(!is.null(invMat)){
+    message("getting cached data")
+    return(invMat)
+  }
+  mat <- x$get()
+  invMat <- solve(mat,...)
+  x$setInverse(invMat)
+  invMat
 }
+
+
+# Testing the Functions
+test_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+test_matrix$get()
+test_matrix$getInverse()
+cacheSolve(test_matrix)
+test_matrix$getInverse()
+test_matrix$set(matrix(c(2, 2, 1, 4), 2, 2))
+test_matrix$get()
+test_matrix$getInverse()
+cacheSolve(test_matrix)
+test_matrix$getInverse()
+
